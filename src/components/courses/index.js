8d77393a0ecf2a12f0a11f921d1716cd44ce2a3e
shaimaamoamen <shaimaamoamen86@gmail.com>
2022-09-15 21:13:34 +0200

    modify

--- src/components/courses/index.js
+++ src/components/courses/index.js
@@ -1,20 +1,68 @@
 import React,{Component} from "react";
-import{CoursesSection,CoursesPart}from "./style.js"
+import{CoursesSection,CoursesPart,Part,Image,Title,Info,Number}from "./style.js"
 class Courses extends Component{
     render(){
         return(
             <CoursesSection>
                 <CoursesPart to="/front">
-                    <img src="images/3.png"/>
+                    <Part>
+                        <center>
+                    < Image src="images/3.jpg"/>
+                        </center>
+                         <Title>
+                            Front-End Development
+                        </Title>
+                        <center>
+                        <Info>
+                            Free<Number>2000 EGP</Number>
+                        </Info>
+                        </center>
+                    </Part>
                 </CoursesPart>
                 <CoursesPart to="/back">
-                    <img src="images/4.png"/>
+                <Part>
+                        <center>
+                    < Image src="images/4.png"/>
+                        </center>
+                         <Title>
+                            Back-End Development
+                        </Title>
+                        <center>
+                        <Info>
+                            Free<Number>750 EGP</Number>
+                        </Info>
+                        </center>
+                    </Part>
                 </CoursesPart>
                 <CoursesPart to="/java">
-                    <img src="images/5.png"/>
+                <Part>
+                        <center>
+                    < Image src="images/5.jpg"/>
+                        </center>
+                         <Title>
+                          Java Mobile Development
+                        </Title>
+                        <center>
+                        <Info>
+                            Free<Number>1500 EGP</Number>
+                        </Info>
+                        </center>
+                    </Part> 
                 </CoursesPart>
                 <CoursesPart to="/ios">
-                    <img src="images/6.png"/>
+                <Part>
+                        <center>
+                    < Image src="images/6.jpg"/>
+                        </center>
+                         <Title>
+                            Ios App Development
+                        </Title>
+                        <center>
+                        <Info>
+                            Free<Number>2500 EGP</Number>
+                        </Info>
+                        </center>
+                    </Part>
                 </CoursesPart>
             </CoursesSection>
         )
